8749cd708d4441f1875a70907f0d3c7bda3784ed
aisaacson1 <50605527+aisaacson1@users.noreply.github.com>
2020-03-04 01:22:56 -0500

    adding dash files and chart lib

--- chart_library.py
+++ chart_library.py
@@ -1,15 +1,3 @@
-import pandas as pd
-import plotly.graph_objects as go
-import plotly.io as pio
-import plotly.express as px
-
-df = pd.read_csv(r"C:\Users\Aaron\Documents\GW_Data_Course\AIsaacson_GW_HW\GW_Project2\sources\Quiz_results_29FEB_noon.csv")
-df.head()
-
-a=df['Data_Type']
-c=df['Chart_Type']
-b=df['Correct']
-
 def bar_function(x,y): 
 
     aggs = ["count","sum","avg","median","mode","rms","stddev","min","max","first","last"]
---
+++ chart_library_test.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.io as pio
+import plotly.express as px
+import chart_library as cl
+
+print(dir(cl))
+
