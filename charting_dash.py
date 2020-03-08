b09002337eb6ccbdafbdfc3b9ac7026fafd9b606
aisaacson1 <50605527+aisaacson1@users.noreply.github.com>
2020-03-08 08:13:17 -0400

    updating dash apps

--- charting_dash.py
+++ charting_dash.py
@@ -14,11 +14,29 @@ import dash_bootstrap_components as dbc
 import pandas as pd
 
 import chart_library as cl
+import decision_tree as dt
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
-app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+app = dash.Dash(__name__,
+                external_stylesheets=external_stylesheets,
+                meta_tags=[{
+                    'name': 'viewport',
+                    'content': 'width=device-width, initial-scale=1.0'
+                    }]
+                )
+
 server = app.server
+
+app.config['suppress_callback_exceptions'] = True
+
+ddoptions = [
+    {"label": "Category", "value": "CAT"},
+    {"label": "Date", "value": "DTE"},
+    {"label": "Value", "value": "VAL"},
+    {"label": "Latitude", "value": "LAT"},
+    {"label": "Longitude", "value": "LON"},
+    ]   
 
 colors = {
     "graphBackground": "#F5F5F5",
@@ -46,18 +64,44 @@ app.layout = html.Div([
         # Allow multiple files to be uploaded
         multiple=True
     ),
+    html.Label("File List"),
+    html.Br(),
+    html.Ul(id="complete-upload"),
 
-    html.Label('Select columns to graph:'),
+    html.Br(),
+    html.Label('Once your data has been uploaded click on "Load Columns"'),
 
-    dcc.Checklist(id='output-column-upload',
-                 labelStyle = dict(display='inline')),
+    html.Br(),
+    html.Button(
+        id='propagate-button',
+        n_clicks=0,
+        children='Load Columns'
+    ),
+    html.Br(),
+    html.Br(),
+    html.Label(id='column-checklist_text'),
+    html.Br(),
+    dcc.Checklist(id='column-checklist',
+                    labelStyle = {
+                        'display': 'inline-block',
+                        'marginRight': 10
+                        }),
 
     # dcc.Graph(id='Mygraph'),
-    html.Div(id='output-data-upload'),
-    # html.Div(id='user_data', style={'display': 'none'}),
-    # html.Div(id='datatable-interactivity-container')
-])
 
+
+    # html.Div(id='display-selected-values'),
+    html.Br(),
+    html.Label(id='data-type-text'),
+    html.Br(),
+    html.Div(id='choosen_columns_data'),
+    html.Br(),
+    html.Div(id='submit_button'),
+    html.Br(),
+    html.Label(id='dropdown-values')
+    ])
+
+##Upload Data
 
 def parse_data(contents, filename):
     content_type, content_string = contents.split(',')
@@ -83,128 +127,242 @@ def parse_data(contents, filename):
 
     return df
 
+##Show when a file is uploaded
 @app.callback(
-                # Output('output-data-upload', 'children'),
-            [
-                Output('output-data-upload', 'children'),
-                Output('output-column-upload', 'options')
-                ],
-            [
+                Output('complete-upload', 'children'),
+
+                [Input('upload-data', 'filename')]
+            )
+
+def upload_complete(filename):
+
+    filename = filename
+
+    if filename is None:
+        return [html.Li("No files have been loaded yet!")]
+    else:
+        return [html.Li(filename) for filename in filename]
+
+
+## Display Select Columns Text
+@app.callback(
+                Output('column-checklist_text', 'children'),
+                [
+                Input('propagate-button', 'n_clicks'),
+                ]
+            )
+
+def update_columns(n_clicks_update):
+
+    if n_clicks_update < 1:
+        print("df empty")
+        return []
+
+    else:
+ 
+        return [html.Label("Select at least two columns to visualize:")]
+
+## Get Column Names when button is clicked and create check list
+@app.callback(
+                Output('column-checklist', 'options'),
+                [
+                Input('propagate-button', 'n_clicks'),
                 Input('upload-data', 'contents'),
                 Input('upload-data', 'filename')
-            ])
-def update_table(contents, filename):
-    table = html.Div()
+                ]
+            )
 
-    if contents:
-        contents = contents[0]
-        filename = filename[0]
-        df = parse_data(contents, filename)
+def update_columns(n_clicks_update, contents, filename):
 
-        table = html.Div([
-            html.H5(filename),
-            dash_table.DataTable(
-                id='user_data',
-                data=df.to_dict('rows'),
-                columns=[{'name': i, 'id': i, "selectable": True} for i in df.columns],
-                fixed_rows={ 'headers': True, 'data': 0 },
-                column_selectable = 'multi',
-                selected_columns=[],
-                # filter_action="native",
-                sort_action="native",
-                sort_mode="multi",
-                page_action="native",
-                page_size= 20,
-                style_table={
-                    'maxHeight': '500px',
-                    # 'overflowY': 'scroll',
-                    'overflowX': 'scroll',
-                },
-                style_cell_conditional=create_conditional_style(df)
-            ),
-        ])
+    if n_clicks_update < 1:
+        print("df empty")
+        return []
 
-        column_head = [{'label': i, 'value': i} for i in df.columns]
-    return table, column_head
-    # return table
+    else:
 
-def create_conditional_style(df):
-    style=[]
-    for col in df.columns:
-        name_length = len(col)
-        pixel = 50 + round(name_length*1)
-        pixel = str(pixel) + "px"
-        style.append({'if': {'column_id': col}, 'minWidth': pixel})
+        if contents:
+            contents = contents[0]
+            filename = filename[0]
+            df = parse_data(contents, filename)
+            column_head = [{'label': i, 'value': i} for i in df.columns]
 
-    return style
+        return column_head
 
+## Display Select Data type Text
+@app.callback(
+                Output('data-type-text', 'children'),
+                [
+                Input('column-checklist', 'value'),
+                ]
+            )
+
+def update_columns(col_value):
+    
+    try:
+        if len(col_value) is None:
+            print("df empty")
+            return []
+        else:
+            return [html.Label("Select the data type for each column, then click submit:")]
+    except:
+        None
+
+# List selected columns with dropdowns for data type
+@app.callback(
+                Output('choosen_columns_data', 'children'),
+                [
+                Input('column-checklist', 'value'),
+                ]
+            )
+
+def create_dropdowns(selected_col):
+
+    ddcreator = []
+
+    try:    
+        for i in selected_col:
+            # if len(ddcreator) >= 0:
+            #     col_width = 100/len(ddcreator)
+            # else:
+            #     col_width = 25
+
+            dd =html.Div(
+                    html.Label(
+                        [i,
+                        dcc.Dropdown(
+                            # id=f"mydropdown{i}",
+                            id="mydropdown1",
+                            options = ddoptions,
+                            persistence_type = 'session',
+                            persistence = True
+                            )
+                        ]
+                    ),
+                style={'width': '20%','marginRight': 10, 'display': 'inline-block'}
+                # style={'width': f'{col_width}%','marginRight': 10, 'display': 'inline-block'}
+                )
+            
+
+            ddcreator.append(dd)
+    except:
+        None
+
+    print(ddcreator)
+    print("-----------------------------")
+    print(len(ddcreator))
+    print("-----------------------------")
+    # print(col_width)
+    # print("-----------------------------")
+
+
+    return ddcreator
+
+## Submit Button
+@app.callback(
+                Output('submit_button', 'children'),
+                [
+                # Input('choosen_columns_data', 'values'),
+                Input('column-checklist', 'value'),
+                ]
+            )
+
+def update_columns(values):
+
+    try:    
+        if len(values) < 1:
+            print("df empty")
+            return []
+        else:
+            button = html.Button(id='submit-button',
+                    n_clicks=0,
+                    children='Submit'
+                    )
+            return button
+    except:
+        None
+
+## Get dropdown values to make data pairs
+@app.callback(
+                Output('dropdown-values', 'values'),
+                [
+                Input('submit-button', 'n_clicks'),
+                Input('choosen_columns_data', 'values'),
+                ],
+                # State('choosen_columns_data', 'values'),
+            )
+
+def update_columns(n_clicks, ddvalues):
+
+    if n_clicks < 1:
+        print("no drop down values")
+        return []
+
+    else:
+        print("-----------------------------")
+        print(ddvalues)
+        print("-----------------------------")
+
+        return ddvalues
+
+# ## Show selected column names
 # @app.callback(
-#                 Output('output-column-upload', 'value')
-#                 ,
-#                 Input('output-column-upload', 'options')
-#             )
-# def set_checks_value(available_options):
-#     return available_options[0]['value']
+#     Output('display-selected-values', 'children'),
+#     [Input('column-checklist', 'value')]
+#     )
+# def set_display_children(selected_column):
+#     return u'{} is the column you selected'.format(
+#         selected_column
+#     )
 
-# @app.callback(
-#                 Output('output-column-upload', 'options')
-#                 ,
-#                 Input('output-data-upload', 'children')
-#             )
-# def set_checks_value(available_options):
-#     return available_options[0]['value']
+# def update_table(contents, filename):
+#     table = html.Div()
 
+#     if contents:
+#         contents = contents[0]
+#         filename = filename[0]
+#         df = parse_data(contents, filename)
+#         column_head = [{'label': i, 'value': i} for i in df.columns]
 
-# @app.callback(
-#     Output('datatable-interactivity-container', "children"),
-#     [Input('user_data', 'selected_columns'),
-#      Input('output-data-upload', "derived_virtual_selected_columns")])
-# def update_graphs(columns, derived_virtual_selected_columns):
-#     # When the table is first rendered, `derived_virtual_data` and
-#     # `derived_virtual_selected_rows` will be `None`. This is due to an
-#     # idiosyncracy in Dash (unsupplied properties are always None and Dash
-#     # calls the dependent callbacks when the component is first rendered).
-#     # So, if `rows` is `None`, then the component was just rendered
-#     # and its value will be the same as the component's dataframe.
-#     # Instead of setting `None` in here, you could also set
-#     # `derived_virtual_data=df.to_rows('dict')` when you initialize
-#     # the component.
-#     if derived_virtual_selected_columns is None:
-#         derived_virtual_selected_columns = []
-
-#     dff = df if columns is None else pd.DataFrame(columns)
-
-#     colors = ['#7FDBFF' if i in derived_virtual_selected_columns else '#0074D9'
-#               for i in range(len(dff))]
-
-#     return [
-#         dcc.Graph(
-#             id=column,
-#             figure={
-#                 "data": [
-#                     {
-#                         "x": dff["Chart_Type"],
-#                         "y": dff[column],
-#                         "type": "bar",
-#                         "marker": {"color": colors},
-#                     }
-#                 ],
-#                 "layout": {
-#                     "xaxis": {"automargin": True},
-#                     "yaxis": {
-#                         "automargin": True,
-#                         "title": {"text": column}
-#                     },
-#                     "height": 250,
-#                     "margin": {"t": 10, "l": 10, "r": 10},
+#         table = html.Div([
+#             html.H5('Select columns to graph:'),
+#             dcc.Checklist(options=column_head,
+#             labelStyle = dict(display='inline')),
+#             html.H5(filename),
+#             dash_table.DataTable(
+#                 id='user_data',
+#                 data=df.to_dict('rows'),
+#                 columns=[{'name': i, 'id': i, "selectable": True} for i in df.columns],
+#                 fixed_rows={ 'headers': True, 'data': 0 },
+#                 column_selectable = 'multi',
+#                 selected_columns=[],
+#                 # filter_action="native",
+#                 sort_action="native",
+#                 sort_mode="multi",
+#                 page_action="native",
+#                 page_size= 20,
+#                 style_table={
+#                     'maxHeight': '500px',
+#                     # 'overflowY': 'scroll',
+#                     'overflowX': 'scroll',
 #                 },
-#             },
-#         )
-#         # check if column exists - user may have deleted it
-# #         # If `column.deletable=False`, then you don't
-# #         # need to do this check.
-# #     #     for column in ["pop", "lifeExp", "gdpPercap"] if column in dff
-#     ]
+#                 style_cell_conditional=create_conditional_style(df)
+#             ),
+#         ])
+#         # print(table)
+#         column_head = [{'label': i, 'value': i} for i in df.columns]
+#     # return table, column_head
+#         # app.layout = serve_layout
+#     return table
+
+# def create_conditional_style(df):
+#     style=[]
+#     for col in df.columns:
+#         name_length = len(col)
+#         pixel = 50 + round(name_length*1)
+#         pixel = str(pixel) + "px"
+#         style.append({'if': {'column_id': col}, 'minWidth': pixel})
+
+#     return style
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ dt_test.py
@@ -0,0 +1,3 @@
+import decision_tree as dt
+
+dt.decision([1,0,1,0,0,0])
